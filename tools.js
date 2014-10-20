63d3f0c87192f9c9023f1cf66244c998072755e3
dukai <dukai86@gmail.com>
2014-10-20 10:35:30 +0800

    update

---
+++ tools.js
@@ -0,0 +1,174 @@
+var isPlainObject = function(obj){
+	if(!obj.hasOwnProperty('constructor') && typeof obj == 'object' && obj.constructor == Object){
+		return true;
+	}
+	
+	return false;
+}
+var mix = function(base, child, deep){
+    var o = new Object();
+    for(var key in base){
+        o[key] = base[key];
+    }
+    for(var key in child){
+		if(deep && isPlainObject(o[key])){
+			o[key] = mix(o[key], child[key]);
+		}else{
+			o[key] = child[key];
+		}
+    }
+    return o;
+};
+
+var extend = function(subClass, baseClass){
+	var parent = subClass.parent = {
+		/**
+		 * parent construct
+		 * @param obj currentObject
+		 * @param args
+		 */
+		'__construct': function(obj, args){
+			baseClass.apply(obj, args);
+		}
+	};
+
+	for(var method in baseClass.prototype){
+		parent[method] = baseClass.prototype[method];
+		if(! (method in subClass.prototype)){
+			subClass.prototype[method] = baseClass.prototype[method];
+		}
+
+	}
+};
+
+var trim = function(str, spliter){
+	var regexp;
+	if(!spliter){
+		regexp = /^(\s|\u00A0)+|(\s|\u00A0)+$/g;
+	}else{
+		regexp = new RegExp("^(" + spliter + ")+|(" + spliter + ")+$", "g");
+	}
+	
+	return str.replace(regexp, '');
+};
+
+
+var dashToUnderScore = function(str){
+	return str.replace(/-/g, '_');
+};
+
+var dashToCamel = function(str){
+	return str.replace(/-([a-z])/, function(matches){
+		return matches[1].toUpperCase();
+	});
+}
+
+var camel2Dash = function(str){
+	return str.replace(/.+([A-Z])/, function(matches){
+		return "-" + matches[1].toLowerCase();
+	});
+};
+
+var eles = {
+	div: document.createElement('div'),
+	ul: document.createElement('ul'),
+	li: document.createElement('li'),
+	span: document.createElement('span'),
+	p: document.createElement('p'),
+	a: document.createElement('a'),
+	fragment: document.createDocumentFragment(),
+	input: document.createElement('input')
+}
+/**
+ * create element
+ * @param tagName
+ * @param id
+ * @param className
+ * @returns {null}
+ */
+var $c = function(tagName, id, className){
+	var ele = null;
+	if(!eles[tagName]){
+		eles[tagName] = document.createElement(tagName);
+		ele = eles[tagName].cloneNode(true);
+	}else{
+		ele = eles[tagName].cloneNode(true);
+	}
+	if(id){
+		ele.id = id;
+	}
+	if(className){
+		ele.className = className;
+	}
+	return ele;
+};
+
+var rand = function (m, n) {
+	return Math.floor((n - m + 1) * Math.random() + m);
+}
+
+var EventEmitter = function(){
+	this._initEventEmitter();
+};
+
+EventEmitter.prototype = {
+	_initEventEmitter: function(){
+		this._events = {};
+	},
+	
+	emit: function(type, eventObject){
+		if(this._events[type]){
+			var events = this._events[type].slice();
+			
+			for(var i = 0, len = events.length; i < len; i++){
+				events[i].listener.call(this, eventObject);
+			}
+		}
+	},
+	
+	addListener: function(type, listener){
+		if(!this._events[type]){
+			this._events[type] = [];
+		}
+		this._events[type].push({
+			type: type,
+			listener: listener
+		});
+	},
+	
+	on: function(type, listener){
+		this.addListener(type, listener);
+	},
+	once: function(type, listener){
+		var fired = false;
+
+		function g(eventObject) {
+			this.removeListener(type, g);
+		
+		    if (!fired) {
+		    	fired = true;
+				listener.apply(this, eventObject);
+			}
+		}
+		
+		  g.listener = listener;
+		  this.on(type, g);
+	},
+	removeListener: function(type, listener){
+		var events = this._events[type];
+		
+		for(var i = 0, len = events.length; i < len; i++){
+			if(events[i] == listener){
+				this._events[type].splice(i, 1);
+			}
+		}
+	},
+	removeAllListeners: function(type){
+		if(type == undefined){
+			this._events = {};
+		}else{
+			delete this._events[type];
+		}
+	},
+	
+};
